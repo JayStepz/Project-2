149e33836692667f95241b0c6ad69903228c911b
Mercy <rogerli19981228@gmail.com>
2023-05-01 19:15:14 -0400

    Base mysql database

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,46 @@
 DROP DATABASE IF EXISTS shopping_db;
 CREATE DATABASE shopping_db;
+
+USE shopping_db;
+
+CREATE TABLE Users (
+  user_id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Products (
+  product_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  category VARCHAR(255),
+  price DECIMAL(10,2) NOT NULL,
+  image_url VARCHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (product_id)
+);
+
+CREATE TABLE Orders (
+  order_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  total_price DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (order_id),
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Order_Items (
+  order_item_id INT NOT NULL AUTO_INCREMENT,
+  order_id INT NOT NULL,
+  product_id INT NOT NULL,
+  quantity INT NOT NULL,
+  unit_price DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (order_item_id),
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
