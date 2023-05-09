7f3ac7bc5be161fdbe07b3115d248b788d241344
Jamtoad <johnathanamayo@gmail.com>
2023-05-09 14:25:54 -0400

    Adds homepage handlebars

--- public/js/product.js
+++ public/js/product.js
@@ -1,20 +1,21 @@
+const score_element = document.querySelector(".score")
+
 async function on_purchase_button_clicked(event){
   const response = await fetch(`/api/users/cart/${event.target.id}`, {
     method : "POST"
   })
   
   if(response.ok){
+    // Guys who doesnt love overloading variables hehe, its my favorite game
     const response = await fetch("/api/users/cart", {
       method : "GET"
     })
     
     if(response.ok){
       response.json().then(function(score){
-        
+        score_element.innerHTML = `Spent: $${score}`
       })
     }
-  }else{
-    
   }
 }
 
