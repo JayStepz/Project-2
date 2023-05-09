3a839c3b948256318b2bebcf098da41e2dd2be0a
Jamtoad <johnathanamayo@gmail.com>
2023-05-09 13:42:17 -0400

    Sets up lots of the backend and routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const withAuth = require('../../utils/auth');
+const product = require("../../models/product")
 
 router.get('/login', (req, res) => {
     res.render('login', {});
@@ -7,31 +8,30 @@ router.get('/login', (req, res) => {
     // Does this need anything else?
 });
 
-router.get('/cart', (req, res) => {
-    // If the user is logged in, continue to Cart.
-    // If the user is not logged in, direct to Log In.
-    if (req.session.logged_in) {
-        res.render('cart');
-        return;
-    }
+router.get("/cart", async function(request, response){
+  try{
+    return response.status(200).json(request.session.score)
+  }catch(error){
+    return response.status(400).json(error)
+  }
+})
 
-    res.render('/login');
-});
-
-// TODO: Only display through shopping cart page and only if the user is logged in?
-router.get('/checkout', (req, res) => {
-    res.render('checkout');
-});
-
-router.get('/orders', (req, res) => {
-    // If the user is logged in, continue to Orders.
-    // If the user is not logged in, direct to Log In.
-    if (req.session.logged_in) {
-        res.render('orders');
-        return;
-    }
-    // TODO: If user is not logged in, redirect to log in page
-    res.render('/login');
-});
+router.post("/cart/:product_id", async function(request, response){
+  try{
+    const {price : product_price} = await product.findByPk(request.params.product_id, {
+      plain : true
+    })
+    
+    request.session.save(function(){
+      request.session.score = request.session.score ? 
+      request.session.score + product_price :
+      product_price
+      
+      return response.status(200).json()
+    })
+  }catch(error){
+    return response.status(400).json(error)
+  }
+})
 
 module.exports = router
--- public/js/product.js
+++ public/js/product.js
@@ -1,1 +1,23 @@
-console.log(document)
+async function on_purchase_button_clicked(event){
+  const response = await fetch(`/api/users/cart/${event.target.id}`, {
+    method : "POST"
+  })
+  
+  if(response.ok){
+    const response = await fetch("/api/users/cart", {
+      method : "GET"
+    })
+    
+    if(response.ok){
+      response.json().then(function(score){
+        
+      })
+    }
+  }else{
+    
+  }
+}
+
+document
+  .querySelector(".purchase_button")
+  .addEventListener("click", on_purchase_button_clicked)
